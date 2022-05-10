74f0bda823f87ecf3d29f47eb731c0a62095813a
Driss <idrissisamm@gmail.com>
2022-05-10 11:14:31 +0100

    Adding Repository

---
+++ DMS.Data/Interfaces/IDepartmentRepository.cs
@@ -0,0 +1,18 @@
+﻿using DMS.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DMS.Data.Interfaces
+{
+    public interface IDepartmentRepository
+    {
+        //PagedResult<Department> GetDepartments(string? name, int page);
+        Task<Department?> GetDepartment(int Id);
+        Task<Department> AddDepartment(Department department);
+        Task<Department?> UpdateDepartment(Department department);
+        Task<Department?> DeleteDepartment(int Id);
+    }
+}
